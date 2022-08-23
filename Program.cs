8870a78369ce2cca6b5838039981fa7e16006693
Narasimha9100 <narasimhaseshani9100@gmail.com>
2022-08-23 22:23:05 +0530

    This is headers of the project

--- Program.cs
+++ Program.cs
@@ -3,14 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace MiniProject
 {
+    public class Headers
+    {
+        public string Continent { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Hotel { get; set; }
+        public int Stars { get; set; }
+        public int Date { get; set; }
+        public int EndDate { get; set; }
+        public int Price { get; set; }
+        public int DiscountedPrice { get; set; }
+        
+    }
     class Program
     {
         static void Main(string[] args)
         {
+           
             Console.WriteLine("Hello team");
+            Console.ReadLine();
         }
     }
 }
