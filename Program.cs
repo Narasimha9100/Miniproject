7c2e57abfc1dfbad68bf8d0b58c2690efd009d08
Narasimha9100 <narasimhaseshani9100@gmail.com>
2022-09-01 09:41:28 +0530

    Add a row by row in Datatabel

--- Program.cs
+++ Program.cs
@@ -30,12 +30,22 @@ namespace MiniProject
                 {
                     DataTable datatable = new DataTable();
                     StreamReader streamreader = new StreamReader(@"D:\Travel Agency Offers.tsv");
-                    char[] delimiter = new char[] { '\t' };
+                    char[] delimiter = new char[] { '\t' }; 
                     string[] columnheaders = streamreader.ReadLine().Split(delimiter);
                     foreach (string columnheader in columnheaders)
                     {
                         datatable.Columns.Add(columnheader); // I've added the column headers here.
                     }
+
+
+                    while (streamreader.Peek() > 0)
+                    {
+                        DataRow datarow = datatable.NewRow();
+                        datarow.ItemArray = streamreader.ReadLine().Split(delimiter);
+                        datatable.Rows.Add(datarow);
+                    }
+
+                    Console.ReadLine();
                 }
 
 
